209c47b9b07064451fcc7012ba918f41f6675bb6
C-Rodg <rodgersbc@gmail.com>
2017-08-07 16:51:42 -0700

    Setup project structure

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return <div className="content">CONTENT</div>;
+	}
+}
+
+export default Content;
