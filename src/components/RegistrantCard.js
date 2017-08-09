8c4ea041e2ef5c0353686656bd6e167fe5704c29
C-Rodg <rodgersbc@gmail.com>
2017-08-09 12:20:41 -0700

    Started sliding accordions

--- src/components/RegistrantCard.js
+++ src/components/RegistrantCard.js
@@ -1,44 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 
-const RegistrantCard = ({ onCloseRegistrant }) => {
-	return (
-		<div className="registrant-card">
-			<div className="card-header attended">
-				<div className="header-info">
-					<div className="info-name text-nowrap">James Tungsten-Walker</div>
-					<div className="info-company text-nowrap">
-						Disney WorldWide Solutions, Inc.
+class RegistrantCard extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			openReg: false,
+			openSessions: false,
+			openExhibitors: false,
+			openFeedback: false
+		};
+	}
+
+	toggleOpen(prop) {
+		this.setState({ [prop]: !this.state[prop] });
+	}
+
+	render() {
+		const { registrant } = this.props;
+		return (
+			<div className="registrant-card">
+				<div
+					className={"card-header " + (registrant.Attended ? "attended" : "")}
+				>
+					<div className="header-info">
+						<div className="info-name text-nowrap">
+							{registrant.FirstName + " " + registrant.LastName}
+						</div>
+						<div className="info-company text-nowrap">
+							{registrant.Company}
+						</div>
+						<div className="info-email text-nowrap">
+							{registrant.Email}
+						</div>
+						<div className="info-status text-nowrap">
+							{registrant.Attended
+								? "Checked-In: " + registrant.FirstCheckInDateTime
+								: "Not Checked-In"}
+						</div>
 					</div>
-					<div className="info-email text-nowrap">
-						jtungstenwalker@disneysolutions.org
+					<div className="header-close" onClick={this.props.onCloseRegistrant}>
+						<i className="material-icons">close</i>
 					</div>
-					<div className="info-status text-nowrap">Not Checked-in</div>
 				</div>
-				<div className="header-close" onClick={onCloseRegistrant}>
-					<i className="material-icons">close</i>
+
+				<div className="card-body">
+					<div
+						className={
+							"card-body-data reg-data " + (this.state.openReg ? "open" : "")
+						}
+					>
+						<div className="body-header">
+							<div className="header-text">Registrant Data</div>
+							<div
+								className="header-icon"
+								onClick={() => this.toggleOpen("openReg")}
+							>
+								<i className="material-icons icon-direction">
+									keyboard_arrow_left
+								</i>
+							</div>
+						</div>
+						<div className="card-body-data-text">Reg data here...</div>
+					</div>
 				</div>
 			</div>
-
-			<div className="card-body">
-				<div className="card-body-data reg-data">
-					<div className="card-body-data-title">Registrant Data</div>
-					<div className="card-body-data-text">Reg data here...</div>
-				</div>
-				<div className="card-body-data session-data">
-					<div className="card-body-data-title">Sessions Attended (6)</div>
-					<div className="card-body-data-text">Sessions data here...</div>
-				</div>
-				<div className="card-body-data exhibitor-data">
-					<div className="card-body-data-title">Exhibitors Visited (4)</div>
-					<div className="card-body-data-text">Exhibitors here...</div>
-				</div>
-				<div className="card-body-data feedback-data">
-					<div className="card-body-data-title">Feedback (2)</div>
-					<div className="card-body-data-text">Feedback here...</div>
-				</div>
-			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default RegistrantCard;
