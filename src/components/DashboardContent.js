b1117ae4d1a19c7fb794ece2ac6f00800a6dc794
C-Rodg <rodgersbc@gmail.com>
2017-08-11 10:05:26 -0700

    Finished small donut component

--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -23,7 +23,7 @@ const DashboardContent = () => {
 					</div>
 				</div>
 				<div className="summary-attendee">
-					<SmallDonut />
+					<SmallDonut value={75} idx={0} />
 					<div className="attendee-info">
 						<div className="attendee-title">Partner</div>
 						<div className="attendee-total">60 attended</div>
@@ -32,7 +32,7 @@ const DashboardContent = () => {
 					</div>
 				</div>
 				<div className="summary-attendee">
-					<SmallDonut />
+					<SmallDonut value={19} idx={1} />
 					<div className="attendee-info">
 						<div className="attendee-title">Customer</div>
 						<div className="attendee-total">35 attended</div>
@@ -41,12 +41,57 @@ const DashboardContent = () => {
 					</div>
 				</div>
 				<div className="summary-attendee">
-					<SmallDonut />
+					<SmallDonut value={87} idx={2} />
 					<div className="attendee-info">
 						<div className="attendee-title">Prospective Developer</div>
-						<div className="attendee-total">7 attended</div>
-						<div className="attendee-sub">7 / 10 pre-registered (70%)</div>
+						<div className="attendee-total">87 attended</div>
+						<div className="attendee-sub">87 / 100 pre-registered (87%)</div>
 						<div className="attendee-sub">0 walk-ins</div>
+					</div>
+				</div>
+				<div className="summary-attendee">
+					<SmallDonut value={53} idx={3} />
+					<div className="attendee-info">
+						<div className="attendee-title">Developer</div>
+						<div className="attendee-total">25 attended</div>
+						<div className="attendee-sub">23 / 45 pre-registered (51%)</div>
+						<div className="attendee-sub">2 walk-ins</div>
+					</div>
+				</div>
+				<div className="summary-attendee">
+					<SmallDonut value={45} idx={4} />
+					<div className="attendee-info">
+						<div className="attendee-title">CEO</div>
+						<div className="attendee-total">12 attended</div>
+						<div className="attendee-sub">2 / 108 pre-registered (2%)</div>
+						<div className="attendee-sub">10 walk-ins</div>
+					</div>
+				</div>
+				<div className="summary-attendee">
+					<SmallDonut value={3} idx={5} />
+					<div className="attendee-info">
+						<div className="attendee-title">C-Level</div>
+						<div className="attendee-total">1 attended</div>
+						<div className="attendee-sub">0 / 33 pre-registered (0%)</div>
+						<div className="attendee-sub">1 walk-ins</div>
+					</div>
+				</div>
+				<div className="summary-attendee">
+					<SmallDonut value={11} idx={6} />
+					<div className="attendee-info">
+						<div className="attendee-title">Manager</div>
+						<div className="attendee-total">0 attended</div>
+						<div className="attendee-sub">0 / 13 pre-registered (0%)</div>
+						<div className="attendee-sub">0 walk-ins</div>
+					</div>
+				</div>
+				<div className="summary-attendee">
+					<SmallDonut value={23} idx={7} />
+					<div className="attendee-info">
+						<div className="attendee-title">Upper Management</div>
+						<div className="attendee-total">44 attended</div>
+						<div className="attendee-sub">23 / 100 pre-registered (23%)</div>
+						<div className="attendee-sub">11 walk-ins</div>
 					</div>
 				</div>
 			</div>
--- src/components/SmallDonut.js
+++ src/components/SmallDonut.js
@@ -1,17 +1,38 @@
 import React from "react";
 
-const SmallDonut = props => {
-	const testVal = 75;
+const attendeeColors = [
+	"#005045",
+	"#C56826",
+	"#5676ac",
+	"#00796b",
+	"#C5A726",
+	"#375b99"
+];
 
+const SmallDonut = props => {
 	const width = 76;
 	const halfSize = width * 0.5;
-	const strokeWidth = 8;
+	const strokeWidth = 10;
 	const radius = halfSize - strokeWidth * 0.5;
 	const circ = Math.PI * 2 * radius;
-	const strokeVal = testVal * circ / 100;
+	const strokeVal = props.value * circ / 100;
 	const dashVal = `${strokeVal}, ${circ}`;
-
 	const rotateVal = `rotate(-90 ${halfSize},${halfSize})`;
+
+	const color = attendeeColors[props.idx % attendeeColors.length];
+	const textStyle = { textAnchor: "middle", fill: color };
+
+	let delay = (props.idx + 1) * 0.5;
+	if (delay > 3) {
+		delay = 3;
+	}
+
+	const animation = `fill-donut 1s cubic-bezier(0.455, 0.03, 0.515, 0.955) ${delay}s forwards`;
+
+	const dashStyles = {
+		strokeDashoffset: strokeVal,
+		animation
+	};
 
 	return (
 		<div className="small-donut">
@@ -32,11 +53,23 @@ const SmallDonut = props => {
 					cy={halfSize}
 					r={radius}
 					fill="transparent"
-					stroke="#ce4b99"
+					stroke={color}
 					strokeWidth={strokeWidth}
 					strokeDasharray={dashVal}
 					transform={rotateVal}
+					style={dashStyles}
 				/>
+				<text
+					x={halfSize + 2}
+					y={halfSize + 8}
+					className="donut-number"
+					style={textStyle}
+				>
+					<tspan style={{ fontSize: "21px" }}>
+						{props.value}
+					</tspan>
+					<tspan style={{ fontSize: "14px" }}>%</tspan>
+				</text>
 			</svg>
 		</div>
 	);
